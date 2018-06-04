a1c6551a3d816badf115835d246ebab84c8785e6
Boris <yarob.ph@gmail.com>
2018-06-04 09:33:08 +0300

    Added maman 23

---
+++ mmn23/rf.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define WRD_SIZE 32
+#define IN 1
+#define OUT 0
+
+int c;
+
+int is_permutation_old(char * word, const char * str) {
+	int i, k, j=0;
+	char perm[strlen(str)];
+	printf("Hello permut: word: %s str: %s\n", word, str);
+	for (i=0; i < strlen(str); i++) {
+		for (k=0; k < strlen(word); k++) {
+			printf("Perm: str[%d]: %c word[%d]: %c\n", i, str[i], k, word[k]);
+			if (str[i] == word[k]) {
+				perm[k] = word[k];
+				break;
+			}
+		}
+		if (k == strlen(word)) {
+			return 0;
+		}
+	}
+	perm[i] = '\0';
+	printf("%s\n", perm);
+	return 1;
+}
+
+int is_permutation(char * word, const char * str) {
+	int i, k, j = 0, state = OUT;
+	char perm[strlen(str)];
+	//printf("Hello permut: word: %s str: %s\n", word, str);
+	for (i = 0, j = 0; word[i] != '\0'; i++) {
+		for (k = 0; str[k] != '\0'; k++) {
+			if (word[i] == str[k]) {
+				state = IN;
+				perm[j++] = word[i];
+			} else if (state == IN) {
+				state = OUT;
+			}
+		}
+		if (j == k) {
+			perm[k] = '\0';
+			printf("%s\n", perm);
+		}
+	}
+	
+	return 1;
+}
+
+void main(int argc, char *argv[]) {
+
+	const char *inputf = argv[1];
+	const char *str = argv[2];
+	int state = OUT, i = 0;
+	int permlen = strlen(str), wordl, permc = 0;
+	char word[WRD_SIZE];
+
+	FILE *f = fopen(inputf, "r");
+
+	if (f == NULL) {
+		printf("Error opening file.\n");
+		exit(1);
+	}
+
+	while ( (c = fgetc(f)) != EOF ) {
+		if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+			//printf("rf 1.0: i: %d c: %d\n", i, c);
+			state = IN;
+			word[i] = c;
+			i++;
+		} else if (state == IN) {
+			//printf("rf 1.1: i: %d\n", i);
+			word[i]='\0';
+			state = OUT;
+			i=0;
+			wordl = strlen(word);
+			if (wordl >= permlen) {
+				//printf("Enter permutation: len: %d\n", wordl);
+				permc += (is_permutation(word, str));
+			}
+		}
+	}
+}
